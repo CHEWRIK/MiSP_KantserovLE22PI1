f80acc3b15b6a3a6605c1464170aba67aad377ff
CHEWRIK <150577969+CHEWRIK@users.noreply.github.com>
2023-12-22 22:27:32 +0300

    Add files via upload

---
+++ html/search/all_0.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['connect_0',['Connect',['../classConnect.html',1,'']]],
+  ['connect_2ecpp_1',['Connect.cpp',['../Connect_8cpp.html',1,'']]],
+  ['connect_5fto_5fserver_2',['Connect_to_server',['../classConnect.html#a347a54af857f70bc997a34b97397663b',1,'Connect']]],
+  ['cons_5ffix_3',['Cons_fix',['../structCons__fix.html',1,'']]]
+];
---
+++ html/search/files_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['connect_2ecpp_0',['Connect.cpp',['../Connect_8cpp.html',1,'']]]
+];
